4aa6d02a93a54e96fad0f86b3581c2a21e224ed6
nishantbothra <83067785+nishantbothra@users.noreply.github.com>
2021-09-24 15:50:28 +0530

    Add files via upload

---
+++ twitter.js
@@ -0,0 +1,9 @@
+function addUser() {
+    var user_name = document.getElementById("user_name").value;
+    localStorage.setItem("user_name", user_name);
+    window.location = "twitter_room.html";
+}
+
+
+
+
